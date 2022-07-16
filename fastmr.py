5cf92941a863e6f865ab7e94141d475226413ad9
wxwmd <706461857@qq.com>
2022-07-16 13:30:28 +0800

    自己修改fastmr的程序

--- fastmr.py
+++ fastmr.py
@@ -2,7 +2,7 @@ import os
 import sys
 from configparser import ConfigParser
 
-import mracc_cluster
+import mracc
 
 
 def main():
@@ -13,19 +13,19 @@ def main():
     config = ConfigParser()
     config.read(conf_path, encoding='UTF-8')
     skip_setup = config.getboolean('cmd', 'skip_setup')
-    mracc_cluster.def_conf(conf_path)
+    mracc.def_conf(conf_path)
     # 创建集群
     engine = config['engine']['model']
     if engine == 'CDT':
-        job = mracc_cluster.create_cluster()
+        job = mracc.create_cluster()
     if engine == 'DT':
-        job = mracc_cluster.control_cluster()
+        job = mracc.control_cluster()
     # 部署环境
     if not skip_setup:
-        mracc_cluster.setup_pkg(job)
-        mracc_cluster.setup_env(job)
+        mracc.setup_pkg(job)
+        mracc.setup_env(job)
     # 运行测试
-    mracc_cluster.run_tpc(job)
+    mracc.run_tpc(job)
 
 if __name__ == '__main__':
     main()
--- test.py
+++
@@ -1,6 +0,0 @@
-
-print("%20s" % ('asdfasdf',))
-print(f"{'asdfasdf':>20}")
-
-print("%5.2f" % (5.5,))
-print(f"{5.5:5.2f}")
